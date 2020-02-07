92fc2d4a8ea54c91038582a03c39b3caee759603
Rupemgera <Rupemgera@users.noreply.github.com>
2020-02-07 21:21:33 +0800

    add GPU rosenbrock test

--- CTest/GPU_test.cxx
+++ CTest/GPU_test.cxx
@@ -1,4 +1,5 @@
 #include "LatticeCore/Parellel/cuda_export.h"
+#include "LatticeCore/Parellel/cuda_optimization.h"
 #include "conf.h"
 #include "gtest/gtest.h"
 
@@ -7,23 +8,29 @@ TEST(gpu_test, device_info_test) {
   EXPECT_EQ(i, 0);
 }
 
-TEST(gpu_test, vector_additon_test) {
-  constexpr int size = 1000;
-  float a[size];
-  float b[size];
-  float gpu_res, cpu_res;
-  for (int i = 0; i < size; ++i) {
-    a[i] = 1.0;
-    b[i] = 1.0;
+TEST(gpu_test, rosenbrock_test) {
+  constexpr int N = 8000000;
+  float *x = new float[N];
+  for (int i = 0; i < N; ++i) {
+    x[i] = 0.0;
   }
-  auto gpu_add = [a, b, &gpu_res]() { gpu_res = vector_add(a, b, size); };
-  auto cpu_add = [a, b, &cpu_res]() {
-    cpu_res = 0.0;
-    for (int i = 0; i < size; ++i) {
-      cpu_res += a[i] * b[i];
+
+  float c_sum = 0.0;
+  auto rosen_cpu = [x, &c_sum]() {
+    for (int id = 0; id < N - 1; ++id) {
+      c_sum +=
+          (x[id + 1] - x[id] * x[id]) * (x[id + 1] - x[id] * x[id]) * 100.0 +
+          (x[id] - 1.0) * (x[id] - 1.0);
     }
   };
-  std::cout << "GPU time: " << time_check(gpu_add) << " ms\n";
-  std::cout << "CPU time: " << time_check(cpu_add) << " ms\n";
-  EXPECT_NEAR(gpu_res, cpu_res, 1e-9);
+
+  std::cout << "CPU time: " << time_check(rosen_cpu) << "ms\n";
+
+  float g_sum = 0.0;
+  auto rosen_gpu = [x, &g_sum]() { g_sum = Rosenbrock_GPU(x, N); };
+
+  std::cout << "GPU time: " << time_check(rosen_gpu) << "ms\n";
+  delete[] x;
+
+  EXPECT_NEAR(g_sum, c_sum, 1e-9);
 }
